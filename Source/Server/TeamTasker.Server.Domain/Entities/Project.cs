d3f7d298c78ffbf1c8c3a385cb1ab35a9b47bb99
Pafton <ptrojnar3@gmail.com>
2024-03-11 13:01:53 +0100

    Create initial database models with class diagram.

---
+++ Source/Server/TeamTasker.Server.Domain/Entities/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTasker.Server.Domain.Entities
+{
+    public class Project 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<Issue> Issues { get; set; }
+    }
+}
