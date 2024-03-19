37f72c1d28ca670ac5235a2b51dd92cba3e8f226
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-19 20:31:07 +0100

    Create base profile for the startup actions of the project

---
+++ Source/Server/TeamTasker.Server.Application/Profiles/BaseProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamTasker.Server.Application.Dtos;
+using TeamTasker.Server.Domain.Entities;
+
+namespace TeamTasker.Server.Application.Profiles
+{
+    public class BaseProfile: Profile
+    {
+        public BaseProfile()
+        {
+            CreateMap<CreateCommentDto, Comment>();
+            CreateMap<Comment, ReadCommentDto>();
+
+            CreateMap<CreateIssueDto, Issue>();
+            CreateMap<Issue, ReadIssueDto>();
+
+            CreateMap<CreateProjectDto, Project>();
+            CreateMap<Project, ReadProjectDto>();
+
+            CreateMap<CreateTeamDto, Team>();
+            CreateMap<Team, ReadTeamDto>();
+
+            CreateMap <Leader, ReadLeaderDto>();
+
+            CreateMap<CreateUserDto, User>();
+            CreateMap<User, ReadUserDto>();
+            CreateMap<Leader, ReadLeaderDto>();
+            CreateMap<Employee, ReadEmployeeDto>();
+        }
+    }
+}
