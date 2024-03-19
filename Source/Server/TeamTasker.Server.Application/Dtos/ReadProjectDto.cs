6daa998cdbf0a2c0d29a41b8e4d0fb06b57ab922
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-19 20:30:39 +0100

    Add additional dtos for project stuff

---
+++ Source/Server/TeamTasker.Server.Application/Dtos/ReadProjectDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamTasker.Server.Domain.Entities;
+
+namespace TeamTasker.Server.Application.Dtos
+{
+    public class ReadProjectDto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Deadline { get; set; }
+        public bool IsComplete { get; set; }
+        public virtual Team Team { get; set; } = default!;
+        public virtual ICollection<Issue> Issues { get; set; } = default!;
+    }
+}
