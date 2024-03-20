1b0cf475c570c3f74693762aa898e92711944f3a
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-20 07:15:09 +0100

    Implement Project Members module

--- Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/ProjectMembers.tsx
+++ Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/ProjectMembers.tsx
@@ -1,11 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import PreviewMembersTable from "../../components/Modules/ProjectMembers/PreviewMembersTable";
+
 
 export default function ProjectMembers()
 {
     return(
         <>
-            <Box sx={{width: "100%", height: "38rem"}}>
-                Project Members
+            <Box sx={{width: "100%", height: "42rem"}}>
+                <Box sx={{display: "flex", mb: "1.5rem"}}>
+                    <Typography variant="h4" sx={{marginRight: "auto"}}>
+                        Project Members
+                    </Typography>
+                </Box>
+                
+                <PreviewMembersTable/>
             </Box>
         </>
     );
