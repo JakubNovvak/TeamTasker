dbc566256ca20ef40b5d7961d874c6ffeb5f2c88
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-20 07:15:44 +0100

    Add the rest fo the modules views

--- Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/Notifications.tsx
+++ Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/Notifications.tsx
@@ -1,11 +1,17 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import PreviewNotificationsTable from "../../components/Modules/Notifcations/PreviewNotificationsTable";
 
 export default function Notifications()
 {
     return(
         <>
-            <Box sx={{width: "100%", height: "38rem"}}>
-                Notifications
+            <Box sx={{width: "100%", height: "42rem"}}>
+                <Box sx={{display: "flex", mb: "1.5rem"}}>
+                    <Typography variant="h4" sx={{marginRight: "auto"}}>
+                        Notifications
+                    </Typography>
+                </Box>
+                <PreviewNotificationsTable/>
             </Box>
         </>
     );
--- Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/ProjectSettings.tsx
+++ Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/ProjectSettings.tsx
@@ -1,11 +1,15 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function ProjectSettings()
 {
     return(
         <>
-            <Box sx={{width: "100%", height: "38rem"}}>
-                Project Settings
+            <Box sx={{width: "100%", height: "42rem"}}>
+                <Box sx={{display: "flex", mb: "1.5rem"}}>
+                    <Typography variant="h4" sx={{marginRight: "auto"}}>
+                        Project Settings
+                    </Typography>
+                </Box>
             </Box>
         </>
     );
