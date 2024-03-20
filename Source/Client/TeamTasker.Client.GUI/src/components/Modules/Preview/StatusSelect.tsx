897ec93bbb58c795755a9391da87fe3e191ba58a
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-20 01:45:56 +0100

    Create select component for the project's status section in the preview page

---
+++ Source/Client/TeamTasker.Client.GUI/src/components/Modules/Preview/StatusSelect.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+export default function StatusSelect() {
+  const [age, setAge] = React.useState('10');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 230, ml: "1.5rem", mt: "1rem" }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label"></InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label=""
+          onChange={handleChange}
+          
+        >
+          <MenuItem value={10}>✅ On the right path</MenuItem>
+          <MenuItem value={20}>⏺ On hold</MenuItem>
+          <MenuItem value={30}>🟪 Finished</MenuItem>
+          <MenuItem value={40}>❌Critically off the path</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
