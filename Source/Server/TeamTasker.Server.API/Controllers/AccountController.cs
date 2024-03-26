ac5b3f6f8428263f09379c8d78a69a494c165779
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-26 19:02:30 +0100

    Initial Account Controller implementation

---
+++ Source/Server/TeamTasker.Server.API/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeamTasker.Server.Application.Dtos.Users;
+using TeamTasker.Server.Application.Interfaces.Authorization;
+using TeamTasker.Server.Domain.Interfaces;
+using TeamTasker.Server.Infrastructure.Services;
+
+namespace TeamTasker.Server.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IJwtAuthorizationService _jwtService;
+
+        public AccountController(IEmployeeService employeeService, IJwtAuthorizationService jwtService)
+        {
+            _employeeService = employeeService;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost]
+        public ActionResult LoginRequest(LoginDto loginUserDto)
+        {
+            //TODO: Call the authorization service
+
+            return Ok();
+        }
+    }
+}
