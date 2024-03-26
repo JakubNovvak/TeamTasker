347939e22a5c400969128ea6cf386029d92950d7
JakubNovvak <jakubszymonnowak@gmail.com>
2024-03-26 17:53:31 +0100

    Create jwt service and helper class, with verification and generation logic of the jwt tokens

---
+++ Source/Server/TeamTasker.Server.Application/Authorization/JwtHelperClass.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTasker.Server.Application.Authorization
+{
+    static internal class JwtHelperClass
+    {
+        
+    }
+}
