2eb5452384ddf12563c0c8c1456a4d911c29c447
JakubNovvak <jakubszymonnowak@gmail.com>
2024-04-03 02:16:19 +0200

    Add AxiosOption schema, for axios requests, and ReadUserDto for get request

---
+++ Source/Client/TeamTasker.Client.GUI/src/components/Types/ReadUserDto.ts
@@ -0,0 +1,10 @@
+export type ReadUserDto = {
+    id: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    position: string,
+    roleId: 1,
+    isTeamLeader: boolean
+}
