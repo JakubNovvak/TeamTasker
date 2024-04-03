abbf6d258645305a5427ddc1dd31d68a78de7b53
JakubNovvak <jakubszymonnowak@gmail.com>
2024-04-03 02:10:28 +0200

    Add global API methods, to authorize acceess to pages

---
+++ Source/Client/TeamTasker.Client.GUI/src/components/Connection/API/CheckLeaderPermission.tsx
@@ -0,0 +1,34 @@
+import axios, { AxiosProxyConfig, AxiosRequestConfig } from "axios";
+import { useEffect, useState } from "react";
+import { AxiosOptions } from "../../Types/AxiosOptions";
+
+async function FetchData(setUserPermission: React.Dispatch<React.SetStateAction<boolean>>, setSendingState: React.Dispatch<React.SetStateAction<boolean>>)
+{
+    setSendingState(true);
+    try{
+        //CAUTION: there is only one tutor in DB, with no near plans of adding more. Should me changed if needed
+        const response = await axios.get('https://localhost:7014/api/Account/authorize/leader', AxiosOptions);
+        console.log("POST: Respone from API" + response.data);
+        setSendingState(false);
+        setUserPermission(true);
+    
+        //await new Promise(resolve => setTimeout(resolve, 4000));
+        //location.href = "/";
+    }
+    catch(error)
+    {
+        console.error("There was an issue with \"CheckLoggedInPermission\" POST request: ", {error});
+        setUserPermission(false);
+        setSendingState(false);
+    }
+}
+
+export default function CheckLeaderPermission(setUserPermission: React.Dispatch<React.SetStateAction<boolean>>)
+{
+    //const [isUserAuthorized, setIsUserAuthorized] = useState<boolean>(false);
+    const [sendingState, setSendingState] = useState<boolean>(false);
+
+    useEffect(() => {
+        FetchData(setUserPermission, setSendingState);
+    }, []);
+}
