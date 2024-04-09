dd50b2356d3b6c8281d2d9c8f8eed8be42cc006b
JakubNovvak <jakubszymonnowak@gmail.com>
2024-04-09 19:27:53 +0200

    Add manage users module template views

---
+++ Source/Client/TeamTasker.Client.GUI/src/components/Admin/ManageUsers/ManageUsersHome.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import BatteryUnknownIcon from '@mui/icons-material/BatteryUnknown';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { motion } from "framer-motion";
+import { NavLink } from "react-router-dom";
+
+export default function ManageUsersHome()
+{
+    return(
+        <>
+        <Box sx={{width: "93vw", height: "85vh", backgroundColor: "none", marginLeft: "-14vw"}}>
+            <Grid container spacing={12}>
+
+                <Grid item xs={12} sx={{display: "flex"}}>
+                    <Button onClick={() => {location.href = "/admindashboard";}}>
+                        <ArrowBackIcon sx={{color: "black", fontSize: "2rem"}}/>
+                    </Button>
+                    <Typography variant="h4">
+                        Manage Users
+                    </Typography>
+                </Grid>
+
+            <Grid item xs={3}>
+                <NavLink to="/admindashboard/manageusers/createuser" style={{textDecoration: "none"}}>
+                <motion.div whileHover={{scale: 1.05, boxShadow: "7px 8px 54px -6px rgba(0, 0, 0, 0.2)"}}>
+                <Paper elevation={5} sx={{padding: "2rem"}}>
+                    <BatteryUnknownIcon sx={{fontSize: "3rem"}}/>
+                    <Typography>
+                        Create User
+                    </Typography>
+                </Paper>
+                </motion.div>
+                </NavLink>
+            </Grid>
+
+            </Grid>
+        </Box>
+        </>
+    );
+}
