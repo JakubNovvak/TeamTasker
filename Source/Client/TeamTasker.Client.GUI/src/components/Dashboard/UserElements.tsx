347af8771fa8c7c45ef5c09486eb039aeee10984
JakubNovvak <jakubszymonnowak@gmail.com>
2024-04-15 18:50:55 +0200

    Display of navbar options, taking notice of projectId in URL

--- Source/Client/TeamTasker.Client.GUI/src/components/Dashboard/UserElements.tsx
+++ Source/Client/TeamTasker.Client.GUI/src/components/Dashboard/UserElements.tsx
@@ -8,47 +8,47 @@ import { useState } from "react";
 import CheckLeaderPermission from "../Connection/API/CheckLeaderPermission";
 import React from "react";
 import UserAvatarMenu from "./UserAvatarMenu";
+import { useParams } from "react-router-dom";
 
 
-function renderSwitch(pathnName: string): string[]
+function renderSwitch(pathnName: string, projectId: string | undefined): string[]
 {
     const [userPermission, setUserPermission] = useState<boolean>(false);
     CheckLeaderPermission(setUserPermission);
     //TODO: Implement better solution ASAP - this is only a temporary solution
-
     switch(pathnName)
     {
         case "/projectname/test":
             return ["", "", ""];
 
-        case "/projectname/preview":
+        case `/projectname/${projectId}/preview`:
             if(userPermission)
                 return ["Components", "Add", "Remove"];
             else
                 return ["", "", ""];
 
-        case "/projectname/issueslist":
+        case `/projectname/${projectId}/issueslist`:
             return ["Your Issues", "", ""];
 
-        case "/projectname/notifications":
+        case `/projectname/${projectId}/notifications`:
             return ["Mentions", "Shared", ""];
 
-        case "/projectname/projectfeed":
+        case `/projectname/${projectId}/projectfeed`:
             if(userPermission)
                 return ["Publish", "", ""];
             else
                 return ["", "", ""];
 
-        case "/projectname/projectsettings":
+        case `/projectname/${projectId}/projectsettings`:
             return ["", "", ""];
 
-        case "/projectname/usersettings":
+        case `/projectname/usersettings`:
             return ["", "", ""];
 
-        case "/projectname/board":
+        case `/projectname/${projectId}/board`:
             return ["Switch Board", "", ""];
 
-        case "/projectname/projectmembers":
+        case `/projectname/${projectId}/projectmembers`:
             if(userPermission)
                 return ["Manage Users", "Manage Roles", ""];
             else
@@ -62,21 +62,22 @@ function renderSwitch(pathnName: string): string[]
 export default function UserElements()
 {
     let pathName = location.pathname;
+    const { projectId } = useParams<{projectId: string}>();
 
     return(
             <Box display="flex" flexDirection="row" sx={{width: "100%", height: "100%"}}>
                 {/*Left Side of the Navbar*/}
                 <Box display="flex" flexDirection="row" sx={{marginRight: "auto", alignItems: "center"}}>
                     <Typography variant="body1" color="#363b4d" fontWeight={550}>
-                    {renderSwitch(pathName)[0] == "" ? <></> : <ArrowDropDownIcon sx={{color: "#363b4d", pt: "0.6rem"}}/>}{renderSwitch(pathName)[0]}
+                    {renderSwitch(pathName, projectId)[0] == "" ? <></> : <ArrowDropDownIcon sx={{color: "#363b4d", pt: "0.6rem"}}/>}{renderSwitch(pathName, projectId)[0]}
                     </Typography>
 
                     <Typography variant="body1" color="#363b4d" fontWeight={550} sx={{ml: "2rem"}}>
-                        {renderSwitch(pathName)[1] == "" ? <></> : <ArrowDropDownIcon sx={{color: "#363b4d", pt: "0.6rem"}}/>}{renderSwitch(pathName)[1]}
+                        {renderSwitch(pathName, projectId)[1] == "" ? <></> : <ArrowDropDownIcon sx={{color: "#363b4d", pt: "0.6rem"}}/>}{renderSwitch(pathName, projectId)[1]}
                     </Typography>
 
                     <Typography variant="body1" color="#363b4d" fontWeight={550} sx={{ml: "2rem"}}>
-                    {renderSwitch(pathName)[2] == "" ? <></> : <ArrowDropDownIcon sx={{color: "#363b4d", pt: "0.6rem"}}/>}{renderSwitch(pathName)[2]}
+                    {renderSwitch(pathName, projectId)[2] == "" ? <></> : <ArrowDropDownIcon sx={{color: "#363b4d", pt: "0.6rem"}}/>}{renderSwitch(pathName, projectId)[2]}
                     </Typography>
 
                     {/* <Button size="small" variant="contained" sx={{backgroundColor: "#363b4d", ml: "1.5rem"}}>Create</Button> */}
