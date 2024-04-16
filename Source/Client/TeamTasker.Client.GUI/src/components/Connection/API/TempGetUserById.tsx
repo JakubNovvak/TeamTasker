51ff295ec0b6205fb4804ead463aa09ade8d83ad
JakubNovvak <jakubszymonnowak@gmail.com>
2024-04-16 23:07:30 +0200

    Add new temp API request, to get user by id

---
+++ Source/Client/TeamTasker.Client.GUI/src/components/Connection/API/TempGetUserById.tsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { AxiosOptions } from "../../Types/AxiosOptions";
+import { ReadEmployeeDto } from "../../Types/ReadEmployeeDto";
+
+export default async function TempGetUserById(employeeId: number,  setUserAvatar: React.Dispatch<React.SetStateAction<string>>)
+{
+    try {
+        const respone = await axios.get<ReadEmployeeDto>(`https://localhost:7014/api/User/id?id=${employeeId}`, AxiosOptions);
+        console.log(respone.data.avatar);
+        setUserAvatar(respone.data.avatar);
+    } catch (error) {
+        
+    }
+}
