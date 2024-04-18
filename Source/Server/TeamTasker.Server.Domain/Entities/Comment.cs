681ae82d711e0c48fa55ffb7cf0e88795d1941f1
Pafton <ptrojnar3@gmail.com>
2024-04-18 20:17:11 +0200

    Remove relation between Comment-User

--- Source/Server/TeamTasker.Server.Domain/Entities/Comment.cs
+++ Source/Server/TeamTasker.Server.Domain/Entities/Comment.cs
@@ -15,7 +15,6 @@ namespace TeamTasker.Server.Domain.Entities
         public DateTime Created { get; set; } = DateTime.Now;
         public string Content { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
-        public virtual ICollection<User> Users { get; set; } = default!;
         public virtual Issue Issue { get; set; } = default!;
         [ForeignKey("Issue")]
         public int? IssueId { get; set; }
