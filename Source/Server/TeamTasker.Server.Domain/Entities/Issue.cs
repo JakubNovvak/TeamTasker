4a6d0af56a0369b97dff6e31175721799f550986
grzegorz1q <grzegorbaryla@gmail.com>
2024-05-09 00:44:14 +0200

    set issue id depending on the project

--- Source/Server/TeamTasker.Server.Domain/Entities/Issue.cs
+++ Source/Server/TeamTasker.Server.Domain/Entities/Issue.cs
@@ -13,6 +13,7 @@ namespace TeamTasker.Server.Domain.Entities
     {
         [Key]
         public int Id { get; set; }
+        public int ProjectIssueId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime StartDate {get; set; }
