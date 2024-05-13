0fefa7e6aa3e788a0a3262e92e0a0403fb9f9c20
JakubNovvak <jakubszymonnowak@gmail.com>
2024-05-13 16:39:27 +0200

    Divide project preview elements into seperate components called from now on "widgets"

--- Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/ProjectPreview.tsx
+++ Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/ProjectPreview.tsx
@@ -1,15 +1,14 @@
-import { Avatar, Box, Grid, Paper, Typography } from "@mui/material";
-import banner_placeholder from "../../assets/banner_placeholder.png";
-import { Button, Textarea } from "@mui/joy";
-import tempText from "./previewText";
-import StatusSelect from "../../components/Modules/Preview/StatusSelect";
-import { NavLink } from "react-router-dom";
+import { Box, Grid, Paper, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import CheckLeaderPermission from "../../components/Connection/API/CheckLeaderPermission";
 import { ReadProjectDto } from "../../components/Types/ReadProjectDto";
 import { GetCurrentProjectInfo } from "../../components/Modules/API/GetCurrentProjectInfo";
 import { ReadEmployeeDto } from "../../components/Types/ReadEmployeeDto";
 import { GetProjectEmployees } from "../../components/Modules/API/GetProjectEmployees";
+import GettingStartedWidget from "../../components/Modules/Preview/Widgets/GettingStartedWidget";
+import ProjectStatusWidget from "../../components/Modules/Preview/Widgets/ProjectStatusWidget";
+import ProjectDescriptionWidget from "../../components/Modules/Preview/Widgets/ProjectDescriptionWidget";
+import MembersWidget from "../../components/Modules/Preview/Widgets/MembersWidget";
 
 export default function ProjectPreview({projectId}: {projectId: string | undefined})
 {
@@ -36,8 +35,6 @@ export default function ProjectPreview({projectId}: {projectId: string | undefin
     useEffect(() => {
         GetCurrentProjectInfo(projectId, setProject, setSendingState, setSendSucess);
         GetProjectEmployees(projectId, setProjectEmployees);
-        //console.log("[0] " + projectEmployees[0].firstName);
-        //console.log(project.status);
     }, []);
 
     if(projectId == undefined)
@@ -54,15 +51,6 @@ export default function ProjectPreview({projectId}: {projectId: string | undefin
       "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
       "https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg"
     ];
-
-    const statusString: {[key: string]: string} = 
-    {
-        "OnTheRightPath": "All tasks are on schedule. The people involved know their tasks. The system is set up well.",
-        "OnHold": "All work in the project has been put on hold.",
-        "Finished": "",
-        "CriticallyOffThePath": "Project requires further supervision as fast as possible.",
-        default: "There was an issue with displaying status description."
-    }
   
     var index = 0;
   
@@ -96,87 +84,24 @@ export default function ProjectPreview({projectId}: {projectId: string | undefin
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <Paper elevation={3} sx={{height: "20rem", background: "lightgray"}}>
-                            <img src={images[index]} style={{width: "100%", height: "100%"}}/>
-                            {/* <Typography variant="h4">
-                                This is a place for your project's banner.
-                            </Typography> */}
+                            <img alt="This is a place for your project's banner." src={images[index]} style={{width: "100%", height: "100%"}}/>
                         </Paper>
                     </Grid>
 
+                    {/*Getting Started*/}
                     <Grid item xs={6}>
-                    <Paper elevation={3} sx={{display: "flex", flexDirection:"column", height: "40rem", background: "white"}}>
-                            <Typography variant="h6" fontWeight={550} sx={{marginRight: "auto", ml: "1.5rem", mt: "1rem"}}>
-                                Getting Started
-                            </Typography>
-
-                            <Textarea defaultValue={tempText} sx={{width: "90%", ml: "2rem", mt: "1.5rem", height: "30rem"}} variant="plain"/>
-
-                        </Paper>
+                        <GettingStartedWidget />
                     </Grid>
+
                     <Grid item xs={6}>
-                    <Paper elevation={3} sx={{display: "flex", flexDirection:"column", height: "12rem"}}>
-                        <Typography variant="h6" fontWeight={550} sx={{marginRight: "auto", ml: "1.5rem", mt: "1rem"}}>
-                                Project Status
-                        </Typography>
-                        {!sendingState ? <StatusSelect projectStatus={project.status}/> : <></>}
-                        
-                        <Textarea defaultValue={!sendingState ? statusString[project.status] : ""} sx={{width: "97%", ml: "1rem", mt:"1rem", height: "2rem"}} variant="plain"/>
+                        {/*Project Status*/}
+                        <ProjectStatusWidget project={project} sendingState={sendingState} />
 
-                    </Paper>
-                    <Paper elevation={3} sx={{display: "flex", flexDirection:"column", height: "12rem", mt: "2rem"}}>
-                        <Typography variant="h6" fontWeight={550} sx={{marginRight: "auto", ml: "1.5rem", mt: "1rem"}}>
-                                Project Description
-                        </Typography>
+                        {/*Project Desc*/}
+                        <ProjectDescriptionWidget project={project}/>
 
-                        <Textarea defaultValue={project.description} sx={{width: "90%", ml: "2rem", mt: "1.5rem", height: "5rem"}} variant="plain"/>
-
-                    </Paper>
-                    <Paper elevation={3} sx={{display: "flex", flexDirection:"column", height: "12rem", mt: "2rem"}}>
-                        <Typography variant="h6" fontWeight={550} sx={{marginRight: "auto", ml: "1.5rem", mt: "1rem"}}>
-                                Members
-                        </Typography>
-
-                        <Typography variant="body1" fontWeight={550} sx={{marginRight: "auto", ml: "1.5rem", mt: "1rem"}}>
-                                {/* Project Owner */}
-                        </Typography>
-
-                        <Box sx={{display: "flex", flexDirection: "row", ml: "1.5rem", mt: "0.5rem"}}>
-                            {/* <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/1.jpg" sx={{width: "1.5rem", height: "1.5rem"}} />
-                            <Typography variant="body1" fontWeight={500} sx={{marginRight: "auto", mt: "0rem"}}>
-                                Test Testowy
-                            </Typography> */}
-                            <Box sx={{display: "flex", flexDirection: "row"}}>
-                                {projectEmployees.map(employee => (
-                    
-                                employee == null 
-                                ?
-                                <></>
-                                :
-                                <>
-                                    <Avatar alt="Cindy Baker" src={employee.avatar} sx={{width: "1.5rem", height: "1.5rem"}} />
-                                    <Typography variant="body1" fontWeight={500} sx={{marginRight: "auto", mt: "0rem"}}>
-                                        {" "}{employee.firstName} {employee.lastName}{"   "}
-                                    </Typography>
-                                    </>
-                                ))}
-                            </Box>
-                        </Box>
-
-                        <Box sx={{display: "flex", flexDirection: "row", ml: "1.5rem"}}>
-                            {leaderPermission ?
-                                <Button sx={{width: "5rem", mt: "1.5rem"}}>Add+</Button>
-                            :
-                                <></>
-                            }
-                        
-                            <NavLink to="projectmembers" style={{textDecoration: "none"}}>
-                                <Button variant="outlined" sx={{width: "10rem", ml: "1.5rem", mt: "1.5rem"}}>
-                                    Show all members
-                                </Button>
-                            </NavLink>
-                        </Box>
-
-                    </Paper>
+                        {/*Project Members*/}
+                        <MembersWidget projectEmployees={projectEmployees} leaderPermission={leaderPermission} />
                     </Grid>
 
                 </Grid>
