c5db4535d84d21ab91fad2c61c3640bca89c3486
JakubNovvak <jakubszymonnowak@gmail.com>
2024-05-21 16:46:54 +0200

    Replace placeholders with acutal data fetched from the server

--- Source/Client/TeamTasker.Client.GUI/src/components/Modules/ProjectFeed/FeedPostCard.tsx
+++ Source/Client/TeamTasker.Client.GUI/src/components/Modules/ProjectFeed/FeedPostCard.tsx
@@ -1,28 +1,32 @@
 import { Avatar, Box, Grid, Typography } from "@mui/material";
 import FeedPostCommentSection from "./PostComments/FeedPostCommentSection";
+import { ReadIssueDto } from "../../Types/ReadIssuesDto";
+import dayjs from "dayjs";
+import { ReadEmployeeDto } from "../../Types/ReadEmployeeDto";
 
-export default function FeedPostCard({commnetsNumber}: {commnetsNumber: number})
+export default function FeedPostCard({feedPost, projectId, tempProjectEmployees}: {feedPost: ReadIssueDto, projectId: string, tempProjectEmployees: ReadEmployeeDto[]})
 {
+    var trimmedDate = dayjs(feedPost.startDate).format('DD MMMM HH:mm');
     return(
         <>
             {/*Post Content Section*/}
             <Grid item xs={5} mt={"2.5rem"}>
                 <Box display={'flex'} flexDirection={'row'}>
-                    <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/1.jpg" sx={{width: "4rem", height: "4rem"}}/> 
+                    <Avatar alt="Cindy Baker" src={tempProjectEmployees.find(employee => employee.id === feedPost.employeeId)?.avatar} sx={{width: "4rem", height: "4rem"}}/> 
                     <Typography fontSize={20} fontWeight={550} sx={{alignSelf: "center", ml: "1rem", textAlign: "left"}}>
-                        There will be changes in the project management team.
+                        {feedPost.name}
                     </Typography>
                 </Box>
 
                 <Box display={'flex'} flexDirection={'row'} sx={{mt: "1.0rem"}}>
-                    <Typography fontSize={14} fontWeight={500} sx={{alignSelf: "center", ml: "0rem", fontStyle: 'italic'}}>
-                        Added by Test Testowy 3 hours ago.
+                    <Typography color={"gray"} fontSize={14} fontWeight={500} sx={{alignSelf: "center", ml: "0rem", fontStyle: 'italic'}}>
+                        Added by Test Testowy on {trimmedDate}.
                     </Typography>
                 </Box>
 
                 <Box display={'flex'} flexDirection={'row'} sx={{mt: "1.0rem"}}>
                     <Typography fontSize={14} fontWeight={500} sx={{alignSelf: "center", ml: "0rem", textAlign: "left"}}>
-                        Hello Team - I just wanted to let you know that our HR department has decided to make some changes in the project management team. Don't worry, it's nothing serious - there's no need to panic or be afraid of losing your job. As developers, you shouldn't be affected by anything done with the project management team.
+                        {feedPost.description}
                     </Typography>
                 </Box>
             </Grid>
@@ -31,7 +35,7 @@ export default function FeedPostCard({commnetsNumber}: {commnetsNumber: number})
             
             {/*Comments Section*/}
             <Grid item xs={5}>
-                <FeedPostCommentSection commnetsNumber={commnetsNumber} />
+                <FeedPostCommentSection postId={feedPost.id} projectId={projectId} tempAvatarUrl={tempProjectEmployees.find(employee => employee.id === feedPost.employeeId)?.avatar ?? ""} />
             </Grid>
 
             <Grid item xs={7}></Grid>
--- Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/ProjectFeed.tsx
+++ Source/Client/TeamTasker.Client.GUI/src/pages/DrawerModules/ProjectFeed.tsx
@@ -1,10 +1,47 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, CircularProgress, Grid, Typography } from "@mui/material";
 import FeedPostCard from "../../components/Modules/ProjectFeed/FeedPostCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
+import { ReadIssueDto } from "../../components/Types/ReadIssuesDto";
+import { GetProjectFeedPosts } from "../../components/Modules/API/Feed/GetProjectFeedPosts";
+import dayjs from "dayjs";
+import { ReadEmployeeDto } from "../../components/Types/ReadEmployeeDto";
+import { GetProjectEmployees } from "../../components/Modules/API/GetProjectEmployees";
 
-export default function ProjectFeed()
+export default function ProjectFeed({projectId}: {projectId: string})
 {
-    const [feedPosts, setFeedPosts] = useState<number[]>([0,3,1,1]);
+    const [sendingState, setSendingState] = useState<boolean>(false);
+    const [sendSucess, setSendSucess] = useState<number>(0);
+
+    //TODO: Temp solution - change to something better
+    const [feedPosts, setFeedPosts] = useState<ReadIssueDto[]>([]);
+
+    const [selectedOption, setSelectedOption] = useState<string>(() => {
+        const storedOption = sessionStorage.getItem('issuesChange');
+        return storedOption ? storedOption : "null";
+      });
+
+      
+      const [tempProjectEmployees, tempSetProjectEmployees] = useState<ReadEmployeeDto[]>([]);
+    useEffect(() => {
+        GetProjectFeedPosts(setSendingState, setSendSucess, setFeedPosts, projectId);
+        GetProjectEmployees(projectId, tempSetProjectEmployees);
+
+        const handleStorageChange = () => {
+            const storedOption = sessionStorage.getItem('feedChange');
+            if (storedOption && storedOption !== selectedOption) {
+              setSelectedOption(storedOption);
+            }
+          };
+          console.log("Changed onSession storage change: " + selectedOption);
+          window.addEventListener('feedChange', handleStorageChange);
+    
+          return () => {
+              window.removeEventListener('feedChange', handleStorageChange);
+              //sessionStorage.removeItem('issuesChange');
+          };
+
+    }, [selectedOption]);
 
     return(
         <>
@@ -15,12 +52,36 @@ export default function ProjectFeed()
                     </Typography>
                 </Box>
 
-                <Grid container>
-                {feedPosts.map(post =>(
-                    <FeedPostCard commnetsNumber={post} />
-                ))}
-                </Grid>
-
+                {sendingState 
+                ?
+                    <CircularProgress />
+                :
+                    <Grid container>
+                    {feedPosts.length == 0 
+                    ?
+                    <Typography variant="h6" sx={{mt: "3rem", display: "flex", alignItems: "center"}}>
+                        <ContentPasteSearchIcon sx={{mr: "01rem"}}/>There are no posts to show...
+                        </Typography>
+                    :
+                        // <>
+                        //     {feedPosts.map(post =>(
+                        //         <FeedPostCard feedPost={post} projectId={projectId} />
+                        //     ))}
+                        // </>
+                        <>
+                            { feedPosts.sort((a, b) => {
+                                //TODO: Change on a server to dates
+                                const dateA = a.id;
+                                const dateB = b.id;
+                                return dateB - dateA;
+                            }).map( post =>(
+                                <FeedPostCard feedPost={post} projectId={projectId} tempProjectEmployees={tempProjectEmployees} />
+                            ))
+                            }
+                        </>
+                    }
+                    </Grid>
+                }
             </Box>
         </>
     );
