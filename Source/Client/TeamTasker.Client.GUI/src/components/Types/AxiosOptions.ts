d2ba1b1ce28aaeb0e97361c74c66993036af4acd
JakubNovvak <jakubszymonnowak@gmail.com>
2024-05-29 05:41:45 +0200

    Updated request body

--- Source/Client/TeamTasker.Client.GUI/src/components/Types/AxiosOptions.ts
+++ Source/Client/TeamTasker.Client.GUI/src/components/Types/AxiosOptions.ts
@@ -3,5 +3,6 @@ import { AxiosRequestConfig } from "axios";
 export const AxiosOptions: AxiosRequestConfig = {
     headers: {
         Authorization: 'Bearer ' + document.cookie.split('; ').filter(row => row.startsWith('JwtToken')).map(c => c.split('=')[1])[0]
-    }
+    },
+    withCredentials: true
 };
